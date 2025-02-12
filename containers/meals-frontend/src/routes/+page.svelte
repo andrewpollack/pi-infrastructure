fbc7ed82c35b86f51fec00e5b7c58a54ffb9a312
andrewpollack <andrewpkq@gmail.com>
2025-02-11 19:38:15 -0800

    meals-frontend
    Breaking out the meal server deployment into a frontend/backend. Frontend
    is in svelte, as I'm interested in learning more and trying it out

---
+++ containers/meals-frontend/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  // Import our new components
+  import CalendarMonth from '$lib/CalendarMonth.svelte';
+  import MealsList from '$lib/MealsList.svelte';
+
+  export let data: PageData;
+
+  // Destructure props for convenience
+  const { allMeals, allCalendars } = data;
+</script>
+
+<h1>Meals</h1>
+
+<!-- Render the "Current" month -->
+<CalendarMonth 
+  monthData={allCalendars.currMonthResponse} 
+/>
+
+<!-- Render the "Next" month -->
+<CalendarMonth 
+  monthData={allCalendars.nextMonthResponse} 
+/>
+
+<!-- Render the "All Meals" list -->
+<MealsList meals={allMeals} />
