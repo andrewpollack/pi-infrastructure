f85d31416e5f609d92e1324e47fd8cf8764e3434
andrewpollack <andrewpkq@gmail.com>
2025-03-26 09:00:01 -0700

    frontend: move navbar to svelte layout entity

--- containers/meals-frontend/src/lib/Navbar.svelte
+++ containers/meals-frontend/src/lib/Navbar.svelte
@@ -1,48 +1,31 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-
-	let { title }: { title: string | null } = $props();
-
-	const icon = '<img src="/favicon.ico" alt="Favicon" style="height: 1em;" />';
 </script>
 
 <div>
-	<ul>
-		<li><a href="/" onclick={() => goto('/')}>Home</a></li>
-		<li><a href="/update" onclick={() => goto('/')}>Update Meals</a></li>
-	</ul>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/update">Update Meals</a>
+	</nav>
 </div>
 
-{#if title}
-	<h1 style="display: flex; align-items: center; justify-content: flex-start; gap: 1rem;">
-		{@html icon}
-		<span>{title}</span>
-		{@html icon}
-	</h1>
-{/if}
-
 <style>
-	ul {
-		list-style-type: none;
-		margin: 0;
-		padding: 0;
-		overflow: hidden;
+	nav {
+		width: 100%;
 		background-color: #b7b1f2;
+		overflow: hidden;
 	}
 
-	li {
+	nav a {
 		float: left;
-	}
-
-	li a {
 		display: block;
 		color: white;
 		text-align: center;
+		text-shadow: black 0.5px 0.5px 0.5px;
 		padding: 14px 16px;
 		text-decoration: none;
 	}
 
-	li a:hover {
+	nav a:hover {
 		background-color: #fdb7ea;
 	}
 </style>
--- containers/meals-frontend/src/routes/+page.svelte
+++ containers/meals-frontend/src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
 	import CalendarMonth from '$lib/CalendarMonth.svelte';
 	import MealsEmail from '$lib/MealsEmail.svelte';
-	import Navbar from '$lib/Navbar.svelte';
+
+	const icon = '<img src="/favicon.ico" alt="Favicon" style="height: 1em;" />';
 
 	let { data } = $props();
-	const { allMeals, currMonthResponse, allEmails } = data;
+	const { allMeals, currMonthResponse, allEmails, allExtraItems } = data;
 </script>
 
-<Navbar title="Meals" />
+<h1 style="display: flex; align-items: center; justify-content: flex-start; gap: 1rem;">
+	{@html icon}
+	<span>Meals</span>
+	{@html icon}
+</h1>
 
 <CalendarMonth monthData={currMonthResponse} />
 
 <div>
-	<MealsEmail meals={allMeals} emails={allEmails} />
+	<MealsEmail meals={allMeals} emails={allEmails} extraItems={allExtraItems} />
 </div>
