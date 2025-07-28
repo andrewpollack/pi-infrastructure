b918415c3a4e4425a35d484ce36a7ce97b7c322d
andrewpollack <andrewpkq@gmail.com>
2025-07-28 16:47:26 -0700

    fix: pr feedback

--- tools/gofmt/check_gofmt.sh
+++ tools/gofmt/check_gofmt.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 # Start from repo root regardless of exec location
 cd "$(git rev-parse --show-toplevel)"
 
-go_files=$(find . -name '*.go' \
+mapfile -t go_files < <(find . -name '*.go' \
   -not -path './bazel-out/*' \
   -not -path './bazel-bin/*' \
   -not -path './bazel-testlogs/*' \
   -not -path './vendor/*')
 
-unformatted=$(gofmt -l $go_files)
+unformatted=$(gofmt -l "${go_files[@]}")
 if [[ -n "$unformatted" ]]; then
   echo "These files are not gofmt'd:"
   echo "$unformatted"
--- tools/golangci-lint/run_golangci-lint.sh
+++ tools/golangci-lint/run_golangci-lint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [[ -z "${BUILD_WORKSPACE_DIRECTORY:-}" ]]; then
+    echo "Error: BUILD_WORKSPACE_DIRECTORY not set" >&2
+    exit 1
+fi
+
 cd "${BUILD_WORKSPACE_DIRECTORY}"
 
 # Run directly in your shell's current directory
